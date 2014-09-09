bbedfceb7ab0f433da5812c9763541c16fb42949
Peter Tseng <patseng@stanford.edu>
2014-09-09 13:33:26 -0700

    Temporary Update to the assignment client side code.

--- client/app/assign/assign.controller.js
+++ client/app/assign/assign.controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('notegoatApp')
-  .controller('AssignCtrl', function ($scope) {
+  .controller('AssignCtrl', function ($scope, $http) {
+  	$http.get('/api/users/').success(function(data, status, headers, config) {
+  		console.log(data);
+  	});
+
     $scope.message = 'Hello';
   });
